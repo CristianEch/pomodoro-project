1f194947d83a89f7d2814983ff2d11993f0fd493
cristian echeverria <clecheverriam@gmail.com>
2025-05-20 05:11:58 -0500

    DB definition syntax update

--- db_definition.sql
+++ db_definition.sql
@@ -1,20 +1,24 @@
 CREATE TABLE user (
-    id_user INT AUTO_INCREMENT PRIMARY KEY, 
+    id_user INT AUTO_INCREMENT, 
     name VARCHAR NOT NULL, 
     password VARCHAR NOT NULL, 
     username VARCHAR NOT NULL, 
-    email VARCHAR NOT NULL)
+    email VARCHAR NOT NULL,
+    PRIMARY KEY (id_user))
 CREATE TABLE activity_status (
-    id_activity_status INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR NOT NULL
+    id_activity_status INT AUTO_INCREMENT,
+    name VARCHAR NOT NULL,
+    PRIMARY KEY(id_activity_status)
 )
 
 CREATE TABLE activity_type (
-    id_activity_type INT AUTO_INCREMENT PRIMARY KEY,
+    id_activity_type INT AUTO_INCREMENT,
     name VARCHAR NOT NULL
+    PRIMARY KEY (id_activity_type)
 )
 CREATE TABLE pomodoro (
-    id_pomodoro INT AUTO_INCREMENT PRIMARY KEY,
+    id_pomodoro INT AUTO_INCREMENT,
+    PRIMARY KEY (id_pomodoro)
     duration INT NOT NULL,
     rest_duration VARCHAR NOT NULL,
     id_activity INT NOT NULL,
@@ -23,31 +27,28 @@ CREATE TABLE pomodoro (
     start_datetime DATETIME NOT NULL,
     id_pomodoro_status INT NOT NULL,
     CONSTRAINT FK_id_pomodoro_status FOREIGN KEY (id_pomodoro_status),
-    REFERENCES pomodoro_status(id_pomodoro_status),
+    REFERENCES pomodoro_status(id_pomodoro_status)
 )    
 CREATE TABLE pomodoro_status (
-    id_pomodoro_status INT AUTO_INCREMENT PRIMARY KEY,
+    id_pomodoro_status INT AUTO_INCREMENT,
     name VARCHAR NOT NULL
+    PRIMARY KEY (id_pomodoro_status)
 )
 
 CREATE TABLE activity(
-    id_activity INT AUTO_INCREMENT PRIMARY KEY,
+    id_activity INT AUTO_INCREMENT,
+    PRIMARY KEY (id_activity)
     estimated_effort VARCHAR NOT NULL,
-    FOREIGN KEY (id_activity_type) REFERENCES activity_type(id_activity_type),
-    FOREIGN KEY (id_activity_status) REFERENCES activity_status(id_activity_status),
-    FOREIGN KEY (id_user) REFERENCES user (id_user),
+    CONSTRAINT FK_id_activity_type FOREIGN KEY (id_activity_type)
+    REFERENCES activity_type(id_activity_type),
+    CONSTRAINT FK_id_activity_status FOREIGN KEY (id_activity_status)
+    REFERENCES activity_status(id_activity_status),
+    CONSTRAINT FK_id_user FOREIGN KEY (id_user)
+    REFERENCES user(id_user),
     start_datetime DATETIME NOT NULL,
     end_datetime DATETIME NOT NULL,
     notes TEXT,
     distractions_count INT,
-    actual_pomodoros INT NOT NULL,
-)
+    actual_pomodoros INT NOT NULL
+);
 
-CREATE TABLE Orders (
-    OrderID int NOT NULL,
-    OrderNumber int NOT NULL,
-    PersonID int,
-    PRIMARY KEY (OrderID),
-    CONSTRAINT FK_PersonOrder FOREIGN KEY (PersonID)
-    REFERENCES Persons(PersonID)
-)
