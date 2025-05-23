a11c8697132df1fe1f0a7dfb03fde5e5d8f05a2b
cristian echeverria <clecheverriam@gmail.com>
2025-05-23 07:40:23 -0500

    fix bd_definition script syntax

--- db_definition.sql
+++ db_definition.sql
@@ -1,54 +1,51 @@
-CREATE TABLE user (
-    id_user INT AUTO_INCREMENT, 
-    name VARCHAR NOT NULL, 
-    password VARCHAR NOT NULL, 
-    username VARCHAR NOT NULL, 
-    email VARCHAR NOT NULL,
-    PRIMARY KEY (id_user))
+CREATE TABLE pomouser (
+    id_user INT AUTO_INCREMENT PRIMARY KEY, 
+    fullname VARCHAR(50) NOT NULL, 
+    password_HASH VARCHAR(255) NOT NULL, 
+    username VARCHAR(50) NOT NULL, 
+    email VARCHAR(50) NOT NULL
+    
+);
 CREATE TABLE activity_status (
-    id_activity_status INT AUTO_INCREMENT,
-    name VARCHAR NOT NULL,
-    PRIMARY KEY(id_activity_status)
-)
+    id_activity_status INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+    
+);
 
 CREATE TABLE activity_type (
     id_activity_type INT AUTO_INCREMENT,
     name VARCHAR NOT NULL
     PRIMARY KEY (id_activity_type)
-)
-CREATE TABLE pomodoro (
-    id_pomodoro INT AUTO_INCREMENT,
-    PRIMARY KEY (id_pomodoro)
-    duration INT NOT NULL,
-    rest_duration VARCHAR NOT NULL,
-    id_activity INT NOT NULL,
-    CONSTRAINT FK_id_activity FOREIGN KEY (id_activity),
-    REFERENCES activity(id_activity),
-    start_datetime DATETIME NOT NULL,
-    id_pomodoro_status INT NOT NULL,
-    CONSTRAINT FK_id_pomodoro_status FOREIGN KEY (id_pomodoro_status),
-    REFERENCES pomodoro_status(id_pomodoro_status)
-)    
-CREATE TABLE pomodoro_status (
-    id_pomodoro_status INT AUTO_INCREMENT,
-    name VARCHAR NOT NULL
-    PRIMARY KEY (id_pomodoro_status)
-)
+);
 
 CREATE TABLE activity(
-    id_activity INT AUTO_INCREMENT,
-    PRIMARY KEY (id_activity)
-    estimated_effort VARCHAR NOT NULL,
-    CONSTRAINT FK_id_activity_type FOREIGN KEY (id_activity_type)
-    REFERENCES activity_type(id_activity_type),
-    CONSTRAINT FK_id_activity_status FOREIGN KEY (id_activity_status)
-    REFERENCES activity_status(id_activity_status),
-    CONSTRAINT FK_id_user FOREIGN KEY (id_user)
-    REFERENCES user(id_user),
+    id_activity INT AUTO_INCREMENT PRIMARY KEY,
+    estimated_effort VARCHAR(50) NOT NULL,
     start_datetime DATETIME NOT NULL,
     end_datetime DATETIME NOT NULL,
     notes TEXT,
     distractions_count INT,
-    actual_pomodoros INT NOT NULL
+    actual_pomodoros INT NOT NULL,
+    id_activity_type INT NOT NULL,
+    id_activity_status INT NOT NULL,
+    id_user INT NOT NULL,
+    CONSTRAINT FK_id_activity_type FOREIGN KEY (id_activity_type) REFERENCES activity_type(id_activity_type),
+    CONSTRAINT FK_id_activity_status FOREIGN KEY (id_activity_status) REFERENCES activity_status(id_activity_status),
+    CONSTRAINT FK_id_user FOREIGN KEY (id_user) REFERENCES pomouser(id_user)
 );
 
+CREATE TABLE pomodoro_status (
+    id_pomodoro_status INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+    
+);
+CREATE TABLE pomodoro (
+    id_pomodoro INT AUTO_INCREMENT PRIMARY KEY,
+    duration INT NOT NULL,
+    rest_duration INT NOT NULL,
+    id_activity INT NOT NULL,
+    start_datetime DATETIME NOT NULL,
+    id_pomodoro_status INT NOT NULL,
+    CONSTRAINT FK_id_activity FOREIGN KEY (id_activity) REFERENCES activity(id_activity),
+    CONSTRAINT FK_id_pomodoro_status FOREIGN KEY (id_pomodoro_status) REFERENCES pomodoro_status(id_pomodoro_status)
+);  
